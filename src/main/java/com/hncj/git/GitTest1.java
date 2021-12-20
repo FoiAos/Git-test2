cbf809d58ab5bbc2f86442596b54541f11f00438
Foi <Foi@hncj.com>
2021-12-20 15:52:14 +0800

    Git first commit

---
+++ src/main/java/com/hncj/git/GitTest1.java
@@ -0,0 +1,11 @@
+package com.hncj.git;
+
+/**
+ * @author yangstart
+ * @create 2021-12-20 15:49
+ */
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("hi !!");
+    }
+}
